018e4ab78f4a26c7a829ed52914631e603ef7040
shivh <shivh@DESKTOP-PEET729>
2021-04-22 12:20:00 +0530

    8

--- src/main/java/com/my/app/SpringBootMvcApplication.java
+++ src/main/java/com/my/app/SpringBootMvcApplication.java
@@ -8,7 +8,6 @@ public class SpringBootMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
-		System.out.println("sop");
 	}
 
 }
